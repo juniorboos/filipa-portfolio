91b07675c3bd038bddf24e0e199d813e1d6443d3
Milton Boos Junior <junior_boos@live.com>
2024-07-14 01:49:08 +0100

    update names

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,19 +6,18 @@ import { Analytics } from "./components/analytics";
 
 export const metadata: Metadata = {
   title: {
-    default: "chronark.com",
-    template: "%s | chronark.com",
+    default: "filipaoliveira.vercel.app",
+    template: "%s | filipaoliveira.vercel.app",
   },
   description: "Co-founder of unkey.dev and founder of planetfall.io",
   openGraph: {
-    title: "chronark.com",
-    description:
-      "Co-founder of unkey.dev and founder of planetfall.io",
-    url: "https://chronark.com",
-    siteName: "chronark.com",
+    title: "filipaoliveira.vercel.app",
+    description: "Co-founder of unkey.dev and founder of planetfall.io",
+    url: "https://filipaoliveira.vercel.app",
+    siteName: "filipaoliveira.vercel.app",
     images: [
       {
-        url: "https://chronark.com/og.png",
+        url: "https://filipaoliveira.vercel.app/og.png",
         width: 1920,
         height: 1080,
       },
@@ -38,7 +37,7 @@ export const metadata: Metadata = {
     },
   },
   twitter: {
-    title: "Chronark",
+    title: "filipaoliveira",
     card: "summary_large_image",
   },
   icons: {
@@ -66,8 +65,9 @@ export default function RootLayout({
         <Analytics />
       </head>
       <body
-        className={`bg-black ${process.env.NODE_ENV === "development" ? "debug-screens" : undefined
-          }`}
+        className={`bg-black ${
+          process.env.NODE_ENV === "development" ? "debug-screens" : undefined
+        }`}
       >
         {children}
       </body>
